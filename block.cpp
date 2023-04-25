e9946ef2bfa30ba93ecfc443617fa78a518870df
1995mazdamiata <nickstorti52@gmail.com>
2023-04-24 20:50:37 -0500

    Fully functional rotation and movement for all blocks

--- block.cpp
+++ block.cpp
@@ -73,6 +73,14 @@ void Block::setType(const blockType type) {
         break;
     case t_shape:
         setColor(PURPLE);
+        xrotation[0] = 1;
+        yrotation[0] = 1;
+        xrotation[1] = 1;
+        yrotation[1] = -1;
+        xrotation[2] = 0;
+        yrotation[2] = 0;
+        xrotation[3] = -1;
+        yrotation[3] = 1;
         pos[0].x = 2*data[0].getSize() + 5;
         pos[1].y = 2*data[0].getSize() + 5;
         pos[2].x = 2*data[0].getSize() + 5;
@@ -82,6 +90,14 @@ void Block::setType(const blockType type) {
         break;
     case z_shape:
         setColor(ORANGE);
+        xrotation[0] = 1;
+        yrotation[0] = -1;
+        xrotation[1] = 0;
+        yrotation[1] = 0;
+        xrotation[2] = -1;
+        yrotation[2] = -1;
+        xrotation[3] = -2;
+        yrotation[3] = 0;
         pos[1].x = 2*data[0].getSize() + 5;
         pos[2].x = 2*data[0].getSize() + 5;
         pos[2].y = 2*data[0].getSize() + 5;
@@ -208,22 +224,84 @@ void Block::move() {
         }
         break;
     case t_shape:
-        if(loc.y <= NUM_ROW - (2*size)) {
+        switch(rotCount) {
+        case 0:
+            if(loc.y <= NUM_ROW - (2*size)) {
+                data[0].move();
+                loc.y++;
+            }
+            data[1].move();
+            data[2].move();
+            data[3].move();
+            break;
+        case 1:
+            if(loc.y <= NUM_ROW - (2*size)) {
+                data[0].move();
+                data[1].move();
+                data[2].move();
+                loc.y++;
+            }
+            data[3].move();
+            break;
+        case 2:
+            if(loc.y <= NUM_ROW - (2*size)) {
+                data[1].move();
+                data[2].move();
+                data[3].move();
+                loc.y++;
+            }
             data[0].move();
-            loc.y++;
+            break;
+        case 3:
+            if(loc.y <= NUM_ROW - (2*size)) {
+                data[0].move();
+                data[2].move();
+                data[3].move();
+                loc.y++;
+            }
+            data[1].move();
+            break;
         }
-        data[1].move();
-        data[2].move();
-        data[3].move();
         break;
     case z_shape:
-        if(loc.y <= NUM_ROW - (2*size)) {
-            data[0].move();
+        switch(rotCount) {
+        case 0:
+            if(loc.y <= NUM_ROW - (2*size)) {
+                data[0].move();
+                data[1].move();
+                loc.y++;
+            }
+            data[2].move();
+            data[3].move();
+            break;
+        case 1:
+            if(loc.y <= NUM_ROW - (2*size)) {
+                data[0].move();
+                data[1].move();
+                data[2].move();
+                loc.y++;
+            }
+            data[3].move();
+            break;
+        case 2:
+            if(loc.y <= NUM_ROW - (2*size)) {
+                data[3].move();
+                data[2].move();
+                loc.y++;
+            }
             data[1].move();
-            loc.y++;
+            data[0].move();
+            break;
+        case 3:
+            if(loc.y <= NUM_ROW - (2*size)) {
+                data[1].move();
+                data[2].move();
+                data[3].move();
+                loc.y++;
+            }
+            data[0].move();
+            break;
         }
-        data[2].move();
-        data[3].move();
         break;
     }
 }
@@ -359,23 +437,89 @@ void Block::moveLeft() {
         break;
     case t_shape:
         if(loc.y <= NUM_ROW - (2*size)) {
-            data[1].moveLeft();
-            if(!data[2].isCollide(data[1])) {
-                data[0].moveLeft();
+            switch(rotCount) {
+            case 0:
+                data[1].moveLeft();
+                if(!data[2].isCollide(data[1])) {
+                    data[0].moveLeft();
+                    data[2].moveLeft();
+                    data[3].moveLeft();
+                    loc.x -= size;
+                }
+                break;
+            case 1:
+                if(loc.y <= NUM_ROW - (3*size)) {
+                data[1].moveLeft();
                 data[2].moveLeft();
                 data[3].moveLeft();
-                loc.x -= size;
+                if(!data[0].isCollide(data[2])) {
+                    data[0].moveLeft();
+                    loc.x -= size;
+                }
+                }
+                break;
+            case 2:
+                data[3].moveLeft();
+                if(!data[2].isCollide(data[3])) {
+                    data[0].moveLeft();
+                    data[1].moveLeft();
+                    data[2].moveLeft();
+                    loc.x -= size;
+                }
+                break;
+            case 3:
+                if(loc.y <= NUM_ROW - (3*size)) {
+                data[0].moveLeft();
+                if(!data[2].isCollide(data[0])) {
+                    data[1].moveLeft();
+                    data[2].moveLeft();
+                    data[3].moveLeft();
+                    loc.x -= size;
+                }
+                }
+                break;
             }
         }
         break;
     case z_shape:
         if(loc.y <= NUM_ROW - (2*size)) {
-            data[0].moveLeft();
-            if(!data[1].isCollide(data[0])) {
-                data[1].moveLeft();
+            switch(rotCount) {
+            case 0:
+                data[0].moveLeft();
+                if(!data[1].isCollide(data[0])) {
+                    data[1].moveLeft();
+                    data[2].moveLeft();
+                    data[3].moveLeft();
+                    loc.x -= size;
+                }
+                break;
+            case 1:
                 data[2].moveLeft();
                 data[3].moveLeft();
-                loc.x -= size;
+                if(!data[1].isCollide(data[2])) {
+                    data[0].moveLeft();
+                    data[1].moveLeft();
+                    loc.x -= size;
+                }
+                break;
+            case 2:
+                data[3].moveLeft();
+                if(!data[2].isCollide(data[3])) {
+                    data[2].moveLeft();
+                    data[1].moveLeft();
+                    data[0].moveLeft();
+                    loc.x -= size;
+                }
+                break;
+            case 3:
+                data[0].moveLeft();
+                data[1].moveLeft();
+                if(!data[2].isCollide(data[1])) {
+                    data[2].moveLeft();
+                    data[3].moveLeft();
+                    loc.x -= size;
+                }
+                break;
             }
         }
         break;
@@ -514,29 +658,95 @@ void Block::moveRight() {
         break;
     case t_shape:
         if(loc.y <= NUM_ROW - (2*size)) {
-            data[3].moveRight();
-            if(!data[2].isCollide(data[3])) {
+            switch(rotCount) {
+            case 0:
+                data[3].moveRight();
+                if(!data[2].isCollide(data[3])) {
+                    data[0].moveRight();
+                    data[1].moveRight();
+                    data[2].moveRight();
+                    loc.x += size;
+                }
+                break;
+            case 1:
+                if(loc.y <= NUM_ROW - (3*size)) {
                 data[0].moveRight();
+                if(!data[2].isCollide(data[0])) {
+                    data[1].moveRight();
+                    data[2].moveRight();
+                    data[3].moveRight();
+                    loc.x += size;
+                }
+                }
+                break;
+            case 2:
+                data[1].moveRight();
+                if(!data[2].isCollide(data[1])) {
+                    data[0].moveRight();
+                    data[2].moveRight();
+                    data[3].moveRight();
+                    loc.x += size;
+                }
+                break;
+            case 3:
+                if(loc.y <= NUM_ROW - (3*size)) {
                 data[1].moveRight();
                 data[2].moveRight();
-                loc.x += size;
+                data[3].moveRight();
+                if(!data[0].isCollide(data[2])) {
+                    data[0].moveRight();
+                    loc.x += size;
+                }
+                }
+                break;
             }
         }
         break;
     case z_shape:
         if(loc.y <= NUM_ROW - (2*size)) {
-            data[3].moveRight();
-            if(!data[2].isCollide(data[3])) {
-                data[2].moveRight();
-                data[1].moveRight();
+            switch(rotCount) {
+            case 0:
+                data[3].moveRight();
+                if(!data[2].isCollide(data[3])) {
+                    data[2].moveRight();
+                    data[1].moveRight();
+                    data[0].moveRight();
+                    loc.x += size;
+                }
+                break;
+            case 1:
                 data[0].moveRight();
-                loc.x += size;
+                data[1].moveRight();
+                if(!data[2].isCollide(data[1])) {
+                    data[2].moveRight();
+                    data[3].moveRight();
+                    loc.x += size;
+                }
+                break;
+            case 2:
+                data[0].moveRight();
+                if(!data[1].isCollide(data[0])) {
+                    data[1].moveRight();
+                    data[2].moveRight();
+                    data[3].moveRight();
+                    loc.x += size;
+                }
+                break;
+            case 3:
+                data[3].moveRight();
+                data[2].moveRight();
+                if(!data[1].isCollide(data[2])) {
+                    data[1].moveRight();
+                    data[0].moveRight();
+                    loc.x += size;
+                }
+                break;
             }
         }
         break;
     }
 }
-//  *NOT FINISHED*
+
 void Block::rotate() {
     point pos[NUM_TILES];
 
@@ -690,8 +900,88 @@ void Block::rotate() {
         }
         break;
     case t_shape:
+        if((loc.y <= NUM_ROW - (2*size)) &&
+          (loc.y >= size) && !((loc.x == 0) &&
+          (rotCount == 1)) && !((loc.x > NUM_COL - (3*size))
+          && (rotCount == 3))) {
+            for(int i = 0; i < NUM_TILES; i++) {
+                    pos[i].x += xrotation[i]*size;
+                    pos[i].y += yrotation[i]*size;
+            }
+            if(rotCount == 0) {
+                xrotation[0] = -1;
+                yrotation[1] = 1;
+                yrotation[3] = -1;
+                loc.x += size;
+                loc.y += size;
+                rotCount++;
+            }
+            else if(rotCount == 1) {
+                yrotation[0] = -1;
+                xrotation[1] = -1;
+                xrotation[3] = 1;
+                loc.x -= size;
+                rotCount++;
+            }
+            else if(rotCount == 2) {
+                xrotation[0] = 1;
+                yrotation[1] = -1;
+                yrotation[3] = 1;
+                rotCount++;
+            }
+            else if(rotCount == 3) {
+                yrotation[0] = 1;
+                xrotation[1] = 1;
+                xrotation[3] = -1;
+                loc.y -= size;
+                rotCount = 0;
+            }
+        }
         break;
     case z_shape:
+        if((loc.y <= NUM_ROW - (2*size)) &&
+          (loc.y >= size) && !((loc.x < size) &&
+          (rotCount == 3)) && !((loc.x > NUM_COL - (4*size))
+          && (rotCount == 1))) {
+            for(int i = 0; i < NUM_TILES; i++) {
+                    pos[i].x += xrotation[i]*size;
+                    pos[i].y += yrotation[i]*size;
+            }
+            if(rotCount == 0) {
+                yrotation[0] = 1;
+                xrotation[2] = 1;
+                xrotation[3] = 0;
+                yrotation[3] = -2;
+                loc.x -= size;
+                rotCount++;
+            }
+            else if(rotCount == 1) {
+                xrotation[0] = -1;
+                yrotation[2] = 1;
+                xrotation[3] = 2;
+                yrotation[3] = 0;
+                loc.x += size;
+                loc.y -= size;
+                rotCount++;
+            }
+            else if(rotCount == 2) {
+                yrotation[0] = -1;
+                xrotation[2] = -1;
+                xrotation[3] = 0;
+                yrotation[3] = 2;
+                loc.x += size;
+                loc.y += size;
+                rotCount++;
+            }
+            else if(rotCount == 3) {
+                xrotation[0] = 1;
+                yrotation[2] = -1;
+                xrotation[3] = -2;
+                yrotation[3] = 0;
+                loc.x -= size;
+                rotCount = 0;
+            }
+        }
         break;
     }
     for(int i = 0; i < NUM_TILES; i++) {
