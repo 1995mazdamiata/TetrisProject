c5101d3faa2ea3fa3811d92de0150f8bddffa455
1995mazdamiata <130611951+1995mazdamiata@users.noreply.github.com>
2023-05-02 20:22:27 -0500

    Update font.h

--- font.h
+++ font.h
@@ -58,12 +58,6 @@ class font{
         int size = 1;
 };
 
-/*This is not quite complete, other
-symbols still need to be added. Also
-below it pulls from a folder called "Fonts"
-so keep that in mind. If you want to use extra
-symbols, you'll have to add them to this function*/
-
 string findFont(char c) {
     string Font;
 
@@ -103,8 +97,5 @@ void displayString(SDL_Plotter& g, string s, int x, int y, int size) {
             }
         }
 }
-
-//curr.init("Fonts\\" + findFont(s.at(i)));
-
 
 #endif // FONT_H_INCLUDED
